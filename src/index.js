100292396fd689cd4bb232b9673f4531b3a35eba
Erastus <erastusngamau8@gmail.com>
2024-06-09 14:44:46 +0300

    list query

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// Initialize QueryClient with default options
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-     {staleTime: 6000,
-      refetchOnWindowFocus:false,
-      retry: 5;
+      staleTime: 6000,
+      refetchOnWindowFocus: false,
+      retry: 5
+    }
+  }
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -21,5 +22,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-
