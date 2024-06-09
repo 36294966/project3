659139b99fee5dbdf46d3a82385a73a72ccfa117
Erastus <erastusngamau8@gmail.com>
2024-06-09 14:36:35 +0300

    updated file of react query.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider, useMutation, useQuery } from "@tanstack/react-query";
+import React from "react";
+
+
+// Initialize QueryClient
+const queryClient = new QueryClient();
 
 function App() {
+  const { data, error, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+        res.json()
+      ),
+    refetchInterval: 4000,
+  });
+
+  const { mutate, isLoading: isPending } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPost),
+      }).then((res) => res.json()),
+    onSuccess: () => {
+      // Invalidate and refetch the "todo" query to update the list
+      queryClient.invalidateQueries("todo");
+    },
+  });
+
+  if (error || isError) return <div>There was an error!</div>;
+  if (isLoading) return <div>DATA IS LOADING...</div>;
+
   return (
     <div className="App">
-      
+      {isPending && <p>DATA IS BEING ADDED...</p>}
+      <button
+        onClick={() =>
+          mutate({
+            userId: 5000,
+            id: 4000,
+            title: "Hey my name is Pedro and this is my Channel!",
+            body: "This is the body of this post",
+          })
+        }
+      >
+        Add Post
+      </button>
+      <div>
+        {data?.map((todo) => (
+          <div key={todo.id}>
+            <h4>ID: {todo.id}</h4>
+            <h4>TITLE: {todo.title}</h4>
+            <p>{todo.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Initialize QueryClient with default options
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+     {staleTime: 6000,
+      refetchOnWindowFocus:false,
+      retry: 5;
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
